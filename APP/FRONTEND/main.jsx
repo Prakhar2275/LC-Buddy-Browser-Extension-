beda8ede871a0026629d0dcd950a858d2fe721c3
Prakhar@2275 <prakharchauhan24853@gmail.com>
2026-03-04 23:28:53 +0530

    intialization of the projecctt jai jagdamb

---
+++ APP/FRONTEND/main.jsx
@@ -0,0 +1,14 @@
+/*
+ * LC Buddy
+ * Copyright (c) 2026 Prakhar Singh Chauhan
+ *
+ * Author: Prakhar Singh Chauhan
+ * Project: LC Buddy
+ *
+ * This file is part of the LC Buddy project.
+ *
+ * Unauthorized copying, modification, distribution, or use of this
+ * software without permission is prohibited.
+ *
+ * This project is maintained as an open-source initiative.
+ */
